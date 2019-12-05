bfe7aeab13c5356f240f559324bbcb6a5ee3afc1
MohamedElmdary <engm5081@gmail.com>
2019-12-05 12:11:37 +0200

    start generate maze

--- src/ts/cell.ts
+++ src/ts/cell.ts
@@ -1,17 +1,14 @@
 import P5 from "p5";
 
 class Cell {
-    public walls: [boolean, boolean, boolean, boolean] = [
-        true,
-        true,
-        true,
-        true
-    ];
-    constructor(public i: number, public j: number) {}
+    // prettier-ignore
+    public walls: [boolean, boolean, boolean, boolean] = [true, true, true, true];
+    public visited: boolean = false;
+    constructor(public x: number, public y: number) {}
 
     public show(w: number, p: P5): void {
-        const x = this.i * w;
-        const y = this.j * w;
+        const x = this.x * w;
+        const y = this.y * w;
         p.stroke(200);
         const wallsEquations = [
             [x + 0, y + 0, x + w, y + 0],
@@ -21,8 +18,30 @@ class Cell {
         ];
 
         wallsEquations
-            .filter((_, i) => this.walls[i])
+            .filter((_, idx) => this.walls[idx])
             .forEach(args => p.line.apply(p, args));
+
+        if (this.visited) {
+            p.fill(255, 0, 255, 100);
+            p.rect(x, y, w, w);
+        }
+    }
+
+    public checkNeighbors(grid: Cell[][], p: P5): Cell | undefined {
+        const neighbors: Cell[] = [
+            [this.x + 0, this.y - 1], // top
+            [this.x + 1, this.y + 0], // right
+            [this.x + 0, this.y + 1], // bottom
+            [this.x - 1, this.y + 0] // left
+        ]
+            .map(([x, y]) => grid?.[y]?.[x])
+            .filter(v => !!v)
+            .filter(v => !v.visited);
+
+        if (neighbors.length) {
+            const idx = p.floor(p.random(0, neighbors.length));
+            return neighbors[idx];
+        }
     }
 }
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,30 +3,40 @@ import { Cell } from "./cell";
 
 let cols: number, rows: number;
 const w = 40;
-const grid: Cell[] = [];
+const grid: Cell[][] = [];
+let current: Cell;
 
 new P5((p: P5) => {
     p.setup = () => {
         console.log("set up...");
+        p.frameRate(5);
         p.createCanvas(400, 400);
         cols = p.floor(p.width / w);
         rows = p.floor(p.height / w);
 
         for (let y = 0; y < rows; y++) {
+            const row: Cell[] = [];
             for (let x = 0; x < cols; x++) {
                 const cell = new Cell(x, y);
-                grid.push(cell);
+                row.push(cell);
             }
+            grid.push(row);
         }
+        current = grid[0][0];
     };
 
     p.draw = () => {
         p.background(51);
-        for (const cell of grid) {
-            cell.show(w, p);
-            // p.stroke(200);
-            // p.noFill();
-            // p.rect(x, y, w, w);
+        for (const row of grid) {
+            for (const cell of row) {
+                cell.show(w, p);
+            }
+        }
+        current.visited = true;
+        let next = current.checkNeighbors(grid, p);
+        if (next) {
+            next.visited = true;
+            current = next;
         }
     };
 });
